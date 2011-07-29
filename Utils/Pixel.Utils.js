5d3f57d3da2fd30c6bb679f3c3d5f54a825e6545
Vtron <steve@vargatron.com>
2011-07-29 17:36:18 -0400

    Not sure what changed here

--- Utils/Pixel.Utils.js
+++ Utils/Pixel.Utils.js
@@ -3,5 +3,26 @@ if(!Pixel) {
 	if(!px) var px = Pixel;
 }
 
-
+//-------------------------------------------------------
 Pixel.isSet = function(item) { return item != undefined ? item : false; };
+
+
+//-------------------------------------------------------
+Pixel.log = function(message) {
+	console.log("Pixel: " + message)
+}
+
+
+//-------------------------------------------------------
+//RequestAnimationFrame finder by Paul Irish
+//from http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
