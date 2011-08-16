b79d018b622d496caf4fba8c1ac66b796536e417
Vtron <steve@vargatron.com>
2011-08-16 11:59:27 -0400

    Added browser types, tween types, object alignment, etc

--- Src/Utils/Pixel.Constants.js
+++ Src/Utils/Pixel.Constants.js
@@ -3,6 +3,12 @@ if(!Pixel) {
 	if(!px) var px = Pixel;
 };
 
+//Browser Type
+Pixel.BROWSER_TYPE_CHROME	= "chrome";
+Pixel.BROWSER_TYPE_SAFARI	= "safari"
+Pixel.BROWSER_TYPE_FIREFOX	= "firefox"
+Pixel.BROWSER_TYPE_IPHONE	= "iphone"
+
 //Render Modes
 Pixel.RENDER_MODE_2D		= 0;
 Pixel.RENDER_MODE_WEBGL		= 1;
@@ -17,3 +23,51 @@ Pixel.TEXT_BASELINE_TOP			= "top";
 Pixel.TEXT_BASELINE_HANGING		= "hanging";
 Pixel.TEXT_BASELINE_MIDDLE		= "middle";
 Pixel.TEXT_BASELINE_BOTTOM		= "bottom";
+
+//Object SHape
+Pixel.OBJECT_SHAPE_RECT		= 0;
+Pixel.OBJECT_SHAPE_CIRCLE	= 1;
+
+//Object Origins
+Pixel.ORIGIN_TOP_LEFT		= 0;
+Pixel.ORIGIN_CENTER_LEFT	= 1;
+Pixel.ORIGIN_BOTTOM_LEFT	= 2;
+
+Pixel.ORIGIN_TOP_RIGHT		= 3;
+Pixel.ORIGIN_CENTER_RIGHT	= 4;
+Pixel.ORIGIN_BOTTOM_RIGHT	= 5;
+
+Pixel.ORIGIN_TOP_CENTER		= 6;
+Pixel.ORIGIN_BOTTOM_CENTER	= 7;
+Pixel.ORIGIN_CENTER			= 8;
+
+//Tween Types
+Pixel.NO_EASE		= TWEEN.Easing.Linear.EaseNone;
+
+Pixel.EASE_IN_QUAD		= TWEEN.Easing.Quadratic.EaseIn;
+Pixel.EASE_OUT_QUAD		= TWEEN.Easing.Quadratic.EaseOut;
+Pixel.EASE_IN_OUT_QUAD	= TWEEN.Easing.Quadratic.EaseInOut;
+
+Pixel.EASE_IN_CUBE		= TWEEN.Easing.Cubic.EaseIn;
+Pixel.EASE_OUT_CUBE		= TWEEN.Easing.Cubic.EaseOut;
+Pixel.EASE_IN_OUT_CUBE	= TWEEN.Easing.Cubic.EaseInOut;
+
+Pixel.EASE_IN_QUART		= TWEEN.Easing.Quartic.EaseIn;
+Pixel.EASE_OUT_QUART	= TWEEN.Easing.Quartic.EaseOut;
+Pixel.EASE_IN_OUT_QUART	= TWEEN.Easing.Quartic.EaseInOut;
+
+Pixel.EASE_IN_QUINT		= TWEEN.Easing.Quintic.EaseIn;
+Pixel.EASE_OUT_QUINT	= TWEEN.Easing.Quintic.EaseOut;
+Pixel.EASE_IN_OUT_QUINT	= TWEEN.Easing.Quintic.EaseInOut;
+
+Pixel.EASE_IN_SIN		= TWEEN.Easing.Sinusoidal.EaseIn;
+Pixel.EASE_OUT_SIN		= TWEEN.Easing.Sinusoidal.EaseOut;
+Pixel.EASE_IN_OUT_SIN	= TWEEN.Easing.Sinusoidal.EaseInOut;
+
+Pixel.EASE_IN_EXPO		= TWEEN.Easing.Exponential.EaseIn;
+Pixel.EASE_OUT_EXPO		= TWEEN.Easing.Exponential.EaseOut;
+Pixel.EASE_IN_OUT_EXPO	= TWEEN.Easing.Exponential.EaseInOut;
+
+Pixel.EASE_IN_CIRC		= TWEEN.Easing.Circular.EaseIn;
+Pixel.EASE_OUT_CIRC		= TWEEN.Easing.Circular.EaseOut;
+Pixel.EASE_IN_OUT_CIRC	= TWEEN.Easing.Circular.EaseInOut;
