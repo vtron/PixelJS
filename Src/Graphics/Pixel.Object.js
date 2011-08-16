7c5f4fa383cca5e9bf14f7819eb62183b15489e4
Vtron <steve@vargatron.com>
2011-08-16 11:54:28 -0400

    Removed reliance on Rect class, too complicated/not needed

--- Src/Graphics/Pixel.Object.js
+++ Src/Graphics/Pixel.Object.js
@@ -1,3 +1,6 @@
+//-------------------------------------------------------
+//-------------------------------------------------------
+//Main Object
 if(!Pixel) {
 	var Pixel = {};
 	if(!px) var px = Pixel;
@@ -11,18 +14,34 @@ Pixel.Object = new Class({
 		y:0
 	},
 	
+	width:0,
+	height:0,
+	
+	radius:0,
+	
+	shapeMode: Pixel.OBJECT_SHAPE_RECT,
+	
+	tween: null,
+	
 	rect:new Pixel.Rectangle,
 	
 	//-------------------------------------------------------
 	initialize:function() {
 	},
 	
+	//-------------------------------------------------------
+	show:function(args) {
+		
+	},
 	
 	//-------------------------------------------------------
-	setPos:function(x,y) {
-		this.rect.setPos(x,y);
-		
-		this.pos = this.rect.getPos();
+	hide:function() {
+	},
+	
+	//-------------------------------------------------------
+	setPos:function(x,y, bTween) {
+		if(Pixel.isSet(x)) this.pos.x = x;
+		if(Pixel.isSet(y)) this.pos.y = y;
 	},
 	
 	
@@ -34,24 +53,27 @@ Pixel.Object = new Class({
 	
 	//-------------------------------------------------------
 	setSize:function(width, height) {
-		this.rect.setSize(width, height);
+		if(Pixel.isSet(width))	this.width	= width;
+		if(Pixel.isSet(height)) this.height = height;
 	},
-	
-	
 	
 	
 	//-------------------------------------------------------
 	getWidth:function() {
-		return this.rect.getSize().width;
+		return this.width;
 	},
 	
 	
 	//-------------------------------------------------------
 	getHeight:function() {
-		return this.rect.getSize().height
+		return this.height
 	},
 	
 	
+	//-------------------------------------------------------
+	setShapeMode: function(shapeMode) {
+		this.shapeMode = shapeMode;
+	},
 	
 	//-------------------------------------------------------
 	//Event Listeners
@@ -61,21 +83,46 @@ Pixel.Object = new Class({
 		return this.bPressed;
 	},
 	
-	//-------------------------------------------------------
-	touchStart:function(touch) {
-		this.bInitPressed	= (this.rect.isInside(touch.pos.x,touch.pos.y));
-		this.bPressed		= this.bInitPressed;
-	},
 	
 	//-------------------------------------------------------
-	touchMoved:function() {
+	touchStart:function(touch) {
+		this.rect.set(this.pos.x, this.pos.y, this.width, this.height);
+		
+		//Touch Detection
+		switch(this.shapeMode) {
+			case Pixel.OBJECT_SHAPE_RECT:
+				this.bInitPressed = (this.rect.isInside(touch.pos.x,touch.pos.y));
+				break;
+			case Pixel.OBJECT_SHAPE_CIRCLE:
+				this.bInitPressed = Pixel.dist(this.pos.x, this.pos.y, touch.pos.x, touch.pos.y) < this.radius * 2;
+				break;
+			default:
+				break;
+		}
+		
+		this.bPressed = this.bInitPressed;
+	},
+	
+	
+	//-------------------------------------------------------
+	touchMoved:function(touch) {
 		if(this.bInitPressed) {
-			this.bPressed = (this.rect.isInside(touch.pos.x,touch.pos.y));
+			switch(this.shapeMode) {
+				case Pixel.OBJECT_SHAPE_RECT:
+					this.bPressed	= (this.rect.isInside(touch.pos.x,touch.pos.y));
+					break;
+				case Pixel.OBJECT_SHAPE_CIRCLE:
+					this.bPressed = Pixel.dist(this.pos.x, this.post.y, touch.pos.x, touch.pos.y) < this.radius * 2;
+					break;
+				default:
+					break;
+			}
 		}
 	},
 	
+	
 	//-------------------------------------------------------
-	touchEnd:function() {
+	touchEnd:function(touch) {
 		this.bInitPressed = this.bPressed = false;
 	}
-})
+});
