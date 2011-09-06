145b80f314e0de6cf3d00dfa15bf0b2ced19906d
Vtron <steve@vargatron.com>
2011-09-06 17:07:49 -0400

    Added Bind() stuff for browsers without function

--- Src/Pixel.js
+++ Src/Pixel.js
@@ -2,6 +2,33 @@
 //Pixel.js
 //Main Class, creates library
 
+//-------------------------------------------------------
+//Define Bind for browsers that don't have it
+//See https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+
+  Function.prototype.bind = function (oThis) {
+
+    if (typeof this !== "function") // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be fBound is not callable");
+
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP ? this : oThis || window, aArgs.concat(Array.prototype.slice.call(arguments)));    
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+
+}
+
+
+//-------------------------------------------------------
 //Create Main Class Object, checking for namespace
 if(typeof Pixel == 'undefined') {
 	Pixel = {
@@ -13,4 +40,7 @@ if(typeof Pixel == 'undefined') {
 } else {
 	console.log("Pixel Namespace Already Exists!");
 }
+
+
+
 
