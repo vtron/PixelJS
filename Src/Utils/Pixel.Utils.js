8365698ea5143e5b62200397c88c920ff2f9b2df
Vtron <steve@vargatron.com>
2011-09-07 01:10:52 -0400

    Fixed mobile/touch detection, added in relative mouse coords (touch coords still needed)

--- Src/Utils/Pixel.Utils.js
+++ Src/Utils/Pixel.Utils.js
@@ -15,12 +15,7 @@ Pixel.log = function(message) {
 //Touch device detection
 //from http://stackoverflow.com/questions/2607248/optimize-website-for-touch-devices
 Pixel.isTouchDevice = function() {
-    try {
-        document.createEvent("TouchEvent");
-        return true;
-    } catch (e) {
-        return false;
-    }
+	return "ontouchstart" in window;
 }
 
 //-------------------------------------------------------
@@ -36,3 +31,26 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
+
+
+//Mouse Coords relative to an element
+//from http://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+Pixel.getRelativeMouseCoords = function(event, element){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = element;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft;
+        totalOffsetY += currentElement.offsetTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+    
+    return {x:canvasX, y:canvasY}
+}
