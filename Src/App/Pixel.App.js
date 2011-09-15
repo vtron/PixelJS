ae411c5f0ceb99fa8a262de9ca46c942b315975e
Vtron <steve@vargatron.com>
2011-09-15 14:42:17 -0400

    Fixed touch events

--- Src/App/Pixel.App.js
+++ Src/App/Pixel.App.js
@@ -29,7 +29,7 @@ Pixel.App = function() {
 	var self = this;
 	
 	if(Pixel.isTouchDevice()) {
-		this.canvas.addEventListener("touchstart",		function(e) { self.touchStartListener.call(self, e) },	false);
+		this.canvas.addEventListener('touchstart',		function(e) { self.touchStartListener.call(self, e) },	false);
 		this.canvas.addEventListener("touchmove",		function(e) { self.touchMovedListener.call(self, e) },	false);
 		this.canvas.addEventListener("touchend",		function(e) { self.touchEndListener.call(self, e) },	false);
 	} else {	
@@ -178,37 +178,33 @@ Pixel.App.prototype.touches = [],
 
 //-------------------------------------------------------
 Pixel.App.prototype.touchStartListener = function(e) {
-	if(this.touchStart != undefined) {
-		for(var i=0;i < e.changedTouches.length; i++) {
-			//Find empty slot for touch
-			var emptyTouchPos = null;
-			for(var j=0; j<this.touches.length; j++) {
-				if(this.touches[j]==null) {
-					emptyTouchPos = j;
-					break;
-				}
+	for(var i=0;i < e.changedTouches.length; i++) {
+		//Find empty slot for touch
+		var emptyTouchPos = null;
+		for(var j=0; j<this.touches.length; j++) {
+			if(this.touches[j]==null) {
+				emptyTouchPos = j;
+				break;
 			}
-			
-			//If slot not found, create new item in touches array (javascript way of doing things)
-			if(emptyTouchPos == null) emptyTouchPos = this.touches.length;
-			
-			//Get the touch position, divide by half if pixel Doubling!
-			var xPos = !this.bPixelDoubling ? e.changedTouches[i].pageX : e.changedTouches[i].pageX/2;
-			var yPos = !this.bPixelDoubling ? e.changedTouches[i].pageY : e.changedTouches[i].pageY/2;
-			
-			//Set the touch
-			this.touches[emptyTouchPos] = {
-				id:j,
-				pos:{
-					x:xPos - this.pos.x,
-					y:yPos - this.pos.y
-				},
-				uniqueID:e.changedTouches[i].identifier
-			}
-			
-			//Deploy Event
-			this.dispatch("touch start", this.touches[emptyTouchPos]);
 		}
+		
+		//If slot not found, create new item in touches array (javascript way of doing things)
+		if(emptyTouchPos == null) emptyTouchPos = this.touches.length;
+		
+		//Get the touch position, divide by half if pixel Doubling!
+		var xPos = !this.bPixelDoubling ? e.changedTouches[i].pageX : e.changedTouches[i].pageX/2;
+		var yPos = !this.bPixelDoubling ? e.changedTouches[i].pageY : e.changedTouches[i].pageY/2;
+		
+		//Set the touch
+		this.touches[emptyTouchPos] = {
+			id:j,
+			x:xPos - this.pos.x,
+			y:yPos - this.pos.y,
+			uniqueID:e.changedTouches[i].identifier
+		}
+		
+		//Deploy Event
+		this.dispatch("touchstart", this.touches[emptyTouchPos]);
 	}
 }
 
@@ -216,30 +212,28 @@ Pixel.App.prototype.touchStartListener = function(e) {
 
 //-------------------------------------------------------
 Pixel.App.prototype.touchMovedListener = function(e) {
-	if(this.touchMoved != undefined) {
-		//Get Changed touches, these are the ones that moved
-		for(var i=0; i<e.changedTouches.length; i++) {
-			//Get each touch's unique ID
-			var uniqueID = e.changedTouches[i].identifier;
+	//Get Changed touches, these are the ones that moved
+	for(var i=0; i<e.changedTouches.length; i++) {
+		//Get each touch's unique ID
+		var uniqueID = e.changedTouches[i].identifier;
+			
+		//Find corresponding touch object
+		for(var j=0; j<this.touches.length;j++) {
+			if(this.touches[j] != null && uniqueID == this.touches[j].uniqueID) {
+				//Get the touch position, divide by half if pixel Doubling!
+				var xPos = !this.bPixelDoubling ? e.changedTouches[i].pageX : e.changedTouches[i].pageX/2;
+				var yPos = !this.bPixelDoubling ? e.changedTouches[i].pageY : e.changedTouches[i].pageY/2;
 				
-			//Find corresponding touch object
-			for(var j=0; j<this.touches.length;j++) {
-				if(this.touches[j] != null && uniqueID == this.touches[j].uniqueID) {
-					//Get the touch position, divide by half if pixel Doubling!
-					var xPos = !this.bPixelDoubling ? e.changedTouches[i].pageX : e.changedTouches[i].pageX/2;
-					var yPos = !this.bPixelDoubling ? e.changedTouches[i].pageY : e.changedTouches[i].pageY/2;
-					
-					
-					//Update touch pos
-					this.touches[j].pos = {
-						x:xPos - this.pos.x,
-						y:yPos - this.pos.y
-					}
-					
-					//Deploy Event
-					this.dispatch("touch moved", this.touches[j]);
-					break;
+				
+				//Update touch pos
+				this.touches[j].pos = {
+					x:xPos - this.pos.x,
+					y:yPos - this.pos.y
 				}
+				
+				//Deploy Event
+				this.dispatch("touchmoved", this.touches[j]);
+				break;
 			}
 		}
 	}
@@ -249,17 +243,15 @@ Pixel.App.prototype.touchMovedListener = function(e) {
 
 //-------------------------------------------------------
 Pixel.App.prototype.touchEndListener = function(e) {
-	if(this.touchEnd != undefined) {
-		for(var i=0; i<e.changedTouches.length; i++) {
-			//Get each touch's unique ID
-			var uniqueID = e.changedTouches[i].identifier;
-			
-			for(var j=0; j<this.touches.length; j++) {
-				if(this.touches[j] != null && uniqueID == this.touches[j].uniqueID) {
-					this.dispatch("touchend", this.touches[j]);
-					this.touches[j] = null;
-					break;
-				}
+	for(var i=0; i<e.changedTouches.length; i++) {
+		//Get each touch's unique ID
+		var uniqueID = e.changedTouches[i].identifier;
+		
+		for(var j=0; j<this.touches.length; j++) {
+			if(this.touches[j] != null && uniqueID == this.touches[j].uniqueID) {
+				this.dispatch("touchend", this.touches[j]);
+				this.touches[j] = null;
+				break;
 			}
 		}
 	}
@@ -281,9 +273,7 @@ Pixel.App.prototype.mouseDownListener = function(e) {
 	var position = Pixel.getRelativeMouseCoords(e, this.canvas);
 		
 	if(this.bIsMobileApp) {
-		if(this.touchStart != undefined) {
-			this.dispatch("touchstart", {id:0, "position":position});
-		}
+		this.dispatch("touchstart", {id:0, x:position.x, y: position.y});
 	} else {
 		this.dispatch("mousedown", position);
 	}
@@ -300,7 +290,7 @@ Pixel.App.prototype.mouseMovedListener = function(e) {
 	var position = Pixel.getRelativeMouseCoords(e, this.canvas);
 			
 	if(this.bIsMobileApp) {
-		this.dispatch("touchmoved", {id:0, "position":position});
+		this.dispatch("touchmoved", {id:0, x:position.x, y: position.y});
 	} else {
 		this.dispatch("mousemoved", position);
 	}
@@ -316,7 +306,7 @@ Pixel.App.prototype.mouseUpListener = function(e) {
 	var position = Pixel.getRelativeMouseCoords(e, this.canvas);
 		
 	if(this.bIsMobileApp) {
-		this.dispatch("touchend", {id:0, "position":position});
+		this.dispatch("touchend", {id:0, x:position.x, y: position.y});
 	} else {
 		this.dispatch("mouseup", position);
 	}
