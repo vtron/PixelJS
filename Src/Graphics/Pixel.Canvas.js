9cd175af4e12589fb2e1b6cc661294d609788d28
Vtron <steve@vargatron.com>
2011-09-15 14:37:54 -0400

    Added all renderer functionality into Canvas, got rid of middle layer

--- Src/Graphics/Pixel.Canvas.js
+++ Src/Graphics/Pixel.Canvas.js
@@ -4,15 +4,13 @@
 //+ generic vars shared between renderers (i.e. Cursor)
 
 Pixel.Canvas = function() {
-	Pixel.Renderer.call(this);
+	Pixel.EventDispatcher(this);
 	
 	//Create Canvas
 	this.canvas = document.createElement('canvas');
 	this.canvas.innerHTML = "Your browser does not support HTML5 Canvas."
 	this.canvas.setAttribute('width', this.width);
 	this.canvas.setAttribute('height', this.height);
-	
-	//this.canvas.webkitTransformOrigin = "0 0 0";
 	
 	this.pos = {
 		x:0,
@@ -37,24 +35,8 @@ Pixel.Canvas = function() {
 	this.setRenderer(this.canvas, Pixel.RENDER_MODE_2D);
 }
 
-Pixel.Canvas.prototype = new Pixel.Renderer();
+Pixel.Canvas.prototype = new Pixel.EventDispatcher();
 
-
-//-------------------------------------------------------
-//Position
-
-//-------------------------------------------------------
-Pixel.Canvas.prototype.setPos = function(x,y) {
-	this.pos.x = x;
-	this.pos.y = y;
-	
-	//Get Transformation (iPhone4 vs 3G/3GS)
-	var transform = "";
-	transform ="scale3d(" + window.devicePixelRatio + "," + window.devicePixelRatio +",0) ";
-	transform += "translate3d(" + this.pos.x + "px, " + this.pos.y + "px,0px)";
-	
-	this.canvas.style.webkitTransform = transform;
-},
 
 
 //-------------------------------------------------------
@@ -96,4 +78,254 @@ Pixel.Canvas.prototype.getHeight = function() {
 Pixel.Canvas.prototype.setCursor = function(x,y) {
 	this.cursorX = x;
 	this.cursorY = y;
+}
+
+
+
+
+
+
+
+//-------------------------------------------------------
+//Drawing
+//-------------------------------------------------------
+Pixel.Canvas.prototype.setRenderer = function(canvasElement, rendererType) {
+	switch(rendererType) {
+		case Pixel.RENDER_MODE_2D:
+			this.renderer = new Pixel.Renderer2D(canvasElement);
+			break;
+		case Pixel.RENDER_MODE_WEBGL:
+			Pixel.log("WebGL Renderer not yet implemented!");
+			break;
+		default:
+			//Pixel.log("Renderer Type does not exist");
+			break;
+	}
+}
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.clear = function(x,y,width,height) { 
+	this.renderer.clear(x,y,width,height); 
+}
+
+
+//-------------------------------------------------------
+//COLOR	
+//-------------------------------------------------------
+Pixel.Canvas.prototype.setColor = function(r,g,b,a) {
+	this.renderer.setColor(r,g,b,a);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.useColor = function(color) {
+	this.renderer.useColor(color);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.fill = function() {
+	this.renderer.fill();
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.noFill = function() {
+	this.renderer.noFill();
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.stroke = function(size) {
+	this.renderer.stroke(size);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.noStroke = function() {
+	this.renderer.noStroke();
+}
+
+
+
+//-------------------------------------------------------
+//IMAGE DRAWING
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawImage = function(pxImage, x, y, width, height) {
+	this.renderer.drawImage(pxImage, x, y);
+}
+
+
+
+//-------------------------------------------------------
+//SHAPE DRAWING
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.beginShape = function(x,y) {
+	this.renderer.beginShape(x,y);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.addVertex = function(x,y, bEnd) {
+	this.renderer.addVertex(x,y, bEnd);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.endShape = function(x,y) {
+	this.renderer.endShape(x,y);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawLine = function(x1,y1,x2,y2) {
+	this.renderer.drawLine(x1,y1,x2,y2);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.dashedLine = function (fromX, fromY, toX, toY, pattern) {
+	this.renderer.dashedLine(fromX, fromY, toX, toY, pattern);  
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawRect = function(x,y,width,height) {
+	this.renderer.drawRect(x,y,width,height);
+}
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawRoundedRect = function(x,y,width,height, borderRadius) {
+	this.renderer.drawRoundedRect(x,y,width,height, borderRadius);
+}
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawSquare = function(x,y,size) {
+	this.renderer.drawSquare(x,y,size);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawEllipse = function(x,y,width,height) {
+	this.renderer.drawEllipse();
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawCircle = function(x,y,radius) {
+	this.renderer.drawCircle(x,y,radius);
+}
+
+
+
+//-------------------------------------------------------
+//TRANSFORMATIONS
+//-------------------------------------------------------
+Pixel.Canvas.prototype.pushMatrix = function() {
+	this.renderer.pushMatrix();
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.popMatrix = function() {
+	this.renderer.popMatrix();
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.translate = function(x,y) {
+	this.renderer.translate(x,y);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.scale = function(x,y) {
+	this.renderer.scale(x,y);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.rotate = function(angle) {
+	this.renderer.rotate(angle);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.transform = function(m11, m12, m21, m22, dx, dy) {
+	this.renderer.transform(m11, m12, m21, m22, dx, dy);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.setTransform = function(m11, m12, m21, m22, dx, dy) {
+	this.renderer.setTransform(m11, m12, m21, m22, dx, dy);
+}
+
+
+
+//-------------------------------------------------------
+//TEXT
+
+//-------------------------------------------------------	
+Pixel.Canvas.prototype.setFont = function(font, size) {
+	this.renderer.setFont(font,size);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.setTextAlignment = function(alignment) {
+	this.renderer.setTextAlignment(alignment);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.setTextBaseline = function(baseline) {
+	this.renderer.setTextBaseline(baseline);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.getTextWidth = function(string) {
+	return this.renderer.getTextWidth(string);
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawText = function(string, x, y) {
+	if(x != undefined) {
+		this.renderer.drawText(string, x, y);
+	} else {
+		this.renderer.drawText(string, this.cursorX, this.cursorY);
+	}
+}
+
+
+
+//-------------------------------------------------------
+Pixel.Canvas.prototype.drawTextfield = function(textfield) {
+	this.renderer.drawTextfield(textfield);
 }
