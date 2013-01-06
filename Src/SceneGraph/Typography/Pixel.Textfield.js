0040c190d4f13079ee29b2f4a16bb0652db8ee32
Vtron <steve@vargatron.com>
2013-01-06 02:41:31 -0500

    Merged layout and textfield into one file, cleaner this way

--- Src/SceneGraph/Typography/Pixel.Textfield.js
+++ Src/SceneGraph/Typography/Pixel.Textfield.js
@@ -4,21 +4,80 @@
 Pixel.TextField = function(font) {
 	Pixel.Shape2D.call(this);
 	
+	//Size
 	this.width	= 100;
 	this.height = 50;
 	
+	this.textWidth	= 0;
+	this.textHeight	= 0;
+	this.useTextBounds	= false; //Use the text bounds vs the frame of the textfield?
+	
+	//Text Properties
 	this.font			= font || new Pixel.Font("Arial", 14);
+	this.leading		= null;
+	
+	this.textAlignment	= Pixel.TEXT_ALIGN_LEFT;
 	this.textColor		= new Pixel.Color(255,255,255,1);
 	this.textSize		= 10;
+	
 	this.text			= "";
 	
-	this.layout			= new Pixel.TextLayout();
+	this.hideOverflow	= false;
+	
+	//Layout
+	this.lines		= [];
+	
 	
 	//Default to transparent BG
 	this.fillEnabled	= false;
 }
 
 Pixel.TextField.prototype = Object.create(Pixel.Shape2D.prototype);
+
+
+
+//-------------------------------------------------------
+//!Size
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.setUseTextBounds = function(useTextBounds) {
+	this.useTextBounds = useTextBounds;
+}
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getUseTextBounds = function(useTextBounds) {
+	return this.useTextBounds;
+}
+
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getWidth = function() {
+	if(this.useTextBounds) {
+		return this.textWidth;
+	} else {
+		return this.width;
+	}
+}
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getHeight = function() {
+	if(this.useTextBounds) {
+		return this.textHeight;
+	} else {
+		return this.height;
+	}
+}
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getBounds = function() {
+	this.calculateOffset();
+	this.bounds.set(this.pos.x + this.offset.x, this.pos.y + this.offset.y, this.getWidth(), this.getHeight());
+	return this.bounds;
+}
+
+
+//-------------------------------------------------------
+//!Text Properties
 
 
 //-------------------------------------------------------
@@ -29,6 +88,7 @@ Pixel.TextField.prototype.setFont = function(font) {
 		Pixel.log("Not a valid Pixel font object.");
 	}
 }
+
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.setTextColor = function(r,g,b,a) {
@@ -44,41 +104,163 @@ Pixel.TextField.prototype.setTextSize = function(size) {
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.setTextAlignment = function(textAlignment) {
-	this.layout.textAlignment = textAlignment;
+	this.textAlignment = textAlignment;
 }
+
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.getTextAlignment = function() {
-	return this.layout.textAlignment;
+	return this.textAlignment;
 }
+
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.setLeading = function(leading) {
-	this.layout.leading = leading;
+	this.leading = leading;
 }
+
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.getLeading = function() {
-	return this.layout.leading;
+	return this.leading;
 }
+
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.setHideOverflow = function(hideOverflow) {
+	this.hideOverflow = hideOverflow;
+}
+
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getText = function(text) {
+	return this.hideOverflow;
+}
+
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.setText = function(text) {
-	this.text = text;
-	
-	if(this.bAutoSize) {
-		this.width	= this.font.getTextWidth(this.text, this.textSize);
-		this.height	= this.font.getTextHeight(this.text, this.textSize);
-	}
-	
-	this.doLayout();
+	this.text = text.toString();
 }
 
 
 //-------------------------------------------------------
-Pixel.TextField.prototype.doLayout = function() {
-	this.layout.doLayout(this.text, this.font, this.textSize, 0, this.width, this.height);
+Pixel.TextField.prototype.getText = function(text) {
+	return this.text;
 }
+
+
+//-------------------------------------------------------
+//!Layout
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.doLayout = function() {
+	this.lines = [];
+	this.textWidth	= 0;
+	this.textHeight = 0;
+	
+	var cursorX = 0,
+		cursorY = 0;
+	
+	var words	= this.text.split(" ");
+	var curLine = this.newLine();
+	
+	var nWords = words.length;
+	for(var i=0; i<nWords; i++) {
+		curLine.width	= this.font.getTextWidth(curLine.text,	this.textSize);
+		var wordWidth	= this.font.getTextWidth(words[i], 		this.textSize);
+		
+		if(curLine.width + wordWidth < this.width) {
+			curLine.text += words[i];
+			if(i!= nWords-1) curLine.text +=   " ";
+		} else {
+			//Get metrics and add line
+			curLine.metrics = this.font.getTextMetrics(curLine.text, this.textSize);
+			curLine.pos.x = this.getLineXPos(curLine.metrics.width);
+			curLine.pos.y = cursorY;
+			this.lines.push(curLine);
+				
+			//Set cursor to next line, including leading
+			//Default leading is 1.2 times the text size until changed manually
+			
+			if(this.leading == null) this.leading = curLine.metrics.height * 1.2;
+			cursorY += curLine.metrics.descent + this.leading;
+			
+			//Check for overflow, 
+			//If it is set and we're past the box we're done
+			var newLineBottom = cursorY +curLine.metrics.descent + this.leading;
+			
+			if(this.hideOverflow &&  newLineBottom > this.height) {
+				this.calculateTextBounds();
+				return;
+			} 
+			
+			//Otherwise, we begin new line		
+			curLine = this.newLine();
+			curLine.text = words[i];
+		}
+	}
+	
+	//Get metrics and add final line
+	curLine.metrics = this.font.getTextMetrics(curLine.text, this.textSize);
+	curLine.pos.x	= this.getLineXPos(curLine.metrics.width);
+	curLine.pos.y	= cursorY;
+	
+	this.lines.push(curLine);
+	
+	//Set the final width & height of the text (no leading)
+	this.calculateTextBounds();
+}
+
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getLineXPos = function(lineWidth) {
+	var xPos = 0;
+	
+	//Set Horizontal position
+	switch(this.textAlignment) {
+		case Pixel.TEXT_ALIGN_LEFT:
+			break;
+		
+		case Pixel.TEXT_ALIGN_CENTER:
+			xPos = this.width/2 - lineWidth/2;
+			break;
+			
+		case Pixel.TEXT_ALIGN_RIGHT:
+			xPos = this.width - lineWidth;
+			break;
+	}
+	
+	return xPos;
+}
+
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.getLines = function() {
+	return this.lines;
+}
+
+
+//-------------------------------------------------------
+Pixel.TextField.prototype.newLine = function() {
+	return {
+		text:"",
+		pos: new Pixel.Point(),
+		metrics: null
+	}
+}
+
+//-------------------------------------------------------
+//Calculate the width of the text box based on the widest line
+Pixel.TextField.prototype.calculateTextBounds = function() {
+	for(var i=0; i<this.lines.length; i++) {
+		this.textWidth = (this.lines[i].metrics.width > this.textWidth) ? this.lines[i].metrics.width : this.textWidth;
+	}
+	
+	var lastLine = this.lines[this.lines.length - 1];
+	this.textHeight = lastLine.pos.y + lastLine.metrics.height;
+}
+
 
 //-------------------------------------------------------
 Pixel.TextField.prototype.draw = function() {
@@ -103,15 +285,16 @@ Pixel.TextField.prototype.draw = function() {
 		}
 		
 		this.calculateOffset();
-		this.canvas.drawRect(this.offset.x, this.offset.y, this.width, this.height);
+		var bounds = this.getBounds();
+		this.canvas.drawRect(this.offset.x, this.offset.y, bounds.width, bounds.height);
 		
 		this.canvas.setFillColor(this.textColor);
 		this.canvas.setFont(this.font.fontFamily, this.textSize);
 		this.canvas.setTextBaseline(this.font.baseline);
 		
-		var nLines = this.layout.getLines().length;;
+		var nLines = this.lines.length;
 		for(var i=0; i<nLines; i++) {
-			var thisLine = this.layout.getLines()[i];
+			var thisLine = this.lines[i];
 			this.canvas.drawText(thisLine.text, this.offset.x + thisLine.pos.x, this.offset.y + thisLine.pos.y);
 		}
 		
