1d1d5b4c97b3af675f691cfa14f91590922a9746
Vtron <steve@vargatron.com>
2013-01-25 00:28:23 -0500

    Starting to test events

---
+++ Test/EventsTest/js/App.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var App = new Pixel.App();
+	
+	App.setSize(1000,500);
+	App.setBackgroundColor(0,0,0);
+	App.showFPS(true);
+	
+	var circle = new Pixel.EllipseShape();
+	circle.setSize(200,200);
+	circle.setAlignment(Pixel.ALIGNMENT_CENTER_CENTER);
+	circle.position.set(App.getWidth()/2, App.getHeight()/2);
+	App.addChild(circle);
+	
+	//----------------------------------------
+	//Update
+	App.update = function() {
+		circle.scaleAmount.x -= 0.01;
+		circle.position.y += 1;
+	}
+	
+	//----------------------------------------
+	//Add to DOM and Init App
+	$(document.body).append(App.element);
+	App.start();
+	
+	App.stop();
+});
